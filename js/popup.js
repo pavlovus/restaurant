d8e478e5eeb3bcd4548804909eb8fadb79ab10ea
Pavlo vus <pavlovus7002@gmail.com>
2022-07-04 17:26:02 +0300

    prefinal

--- js/popup.js
+++ js/popup.js
@@ -4,6 +4,8 @@ let closePopupButton = document.querySelector(".popup__close")
 let popupForm = document.querySelector(".popup__form")
 let popupSubtitle = document.querySelector(".popup__subtitle")
 let popupContent = document.querySelector(".popup__content")
+let inputs = document.querySelectorAll(".popup__input")
+let k = 1;
 openPopupButton.addEventListener('click', (e) => {
     e.preventDefault()
     popup.classList.add('popup--active')
@@ -17,6 +19,17 @@ closePopupButton.addEventListener('click', () => {
 })
 function submitPopupForm(e) {
     e.preventDefault()
+    let data;
+    inputs.forEach((element, index) => {
+        let data = JSON.stringify(element.value)
+        let keys = [
+            "name",
+            "email",
+            "comment"
+        ]
+        localStorage.setItem(keys[index]+ k , data)
+    })
+    k++
     popupForm.classList.add('popup__form--submited')
     popupSubtitle.classList.add('popup__subtitle--submited')
 }
--- js/slider.js
+++ js/slider.js
@@ -2,9 +2,19 @@ const slider = document.querySelector('.slider');
 const btnLeft = document.querySelector('.slider__button--left')
 const btnRight = document.querySelector('.slider__button--right')
 const sliderDots = document.querySelector('.slider__dots')
+let sliderTitle = document.querySelector('.slider__title')
+let sliderText = document.querySelector('.slider__text')
 let imgLinks = [
     "url(/img/bg1.jpg)",
     "url(/img/bg2.jpg)",
+]
+let titles = [
+    "First offer",
+    "Second offer",
+]
+let texts = [
+    "Description of the first offer",
+    "Description of the second offer",
 ]
 let current = 0;
 for (let i = 0; i < imgLinks.length; i++) {
@@ -13,6 +23,8 @@ for (let i = 0; i < imgLinks.length; i++) {
 }
 function render() {
     slider.style.backgroundImage = imgLinks[current];
+    sliderTitle.innerHTML = titles[current]
+    sliderText.innerHTML = texts[current]
     sliderDots.children[current].classList.add('selected')
 }
 render()
